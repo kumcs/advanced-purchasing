43d6f3deb2d5efb2552c899529ac8d60e1048c76
Dave Anderson (xTuple) <danderson@xtuple.com>
2016-04-22 09:22:09 +1200

    Advanced Purchasing: resolve conflict between alternate packages

--- purchauths/client/scripts/purchaseOrderItem.js
+++ purchauths/client/scripts/purchaseOrderItem.js
@@ -2,32 +2,30 @@ include("purchauth");
 purchauth.purchaseorderitem = new Object;
 
 var _unitPrice = mywindow.findChild("_unitPrice");
+var _item = mywindow.findChild("_item");
+var _expcat = mywindow.findChild("_expcat");
 
-var _mode;
-var _po;
-var _poStatus;
-
-function set(input)
-{
-  if ("mode" in input)
-    _mode = input.mode;
-  if ("pohead_id" in input)
-  {
-    _po = input.pohead_id
-    var _pohead = toolbox.executeQuery("SELECT pohead_status FROM pohead WHERE pohead_id=<? value('po') ?>", {po: _po});
-    if (purchauth.errorCheck(_pohead) && _pohead.first())
-     _poStatus = _pohead.value("pohead_status");
-  }
-
-  if (_mode != "new" && _poStatus != "U")
-    handleUnitPricing();
-}
+var _status;
 
 function handleUnitPricing()
 {
   if (!metrics.boolean("UsePurchasingAuths"))
     return;
 
+  var _poitem = mywindow.id();
+
+  var _postatus = toolbox.executeQuery("SELECT pohead_status FROM pohead JOIN poitem ON (poitem_pohead_id=pohead_id) WHERE poitem_id=<? value('poitem') ?>", {poitem: _poitem});
+  if (purchauth.errorCheck(_postatus) && _postatus.first())
+     _status = _postatus.value("pohead_status");
+  else
+    return;
+
+  if (_status == 'U')
+    return;
+
   if (!privileges.check("OverrideReleasedPOPrices"))
     _unitPrice.setEnabled(false);
 }
+
+_item["newId(int)"].connect(handleUnitPricing);
+_expcat["newId(int)"].connect(handleUnitPricing);
